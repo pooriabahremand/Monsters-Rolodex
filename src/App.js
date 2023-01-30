61a31058137268acad463e5d77fcdea121f6f057
pooriabahremand <106535603+pooriabahremand@users.noreply.github.com>
2023-01-30 15:55:26 +0330

    commiting for not letting the changes ruin the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import CardList from "./Component/CardList";
+import "./App.css";
+// import Test from "./test";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          }
+        )
+      );
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const searched = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monster"
+          onChange={onSearchChange}
+        />
+        {/* {searched.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })} */}
+        <CardList monsters = {searched} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/test.js
@@ -0,0 +1,8 @@
+import { Component } from "react";
+class Test extends Component {
+  render() {
+    return <div>this line of code is from test component</div>;
+  }
+}
+
+export default Test;
